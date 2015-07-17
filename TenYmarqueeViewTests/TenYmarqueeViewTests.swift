e13ff43e7d83f07dbf88834f8e6126e2e5dbb36b
tenyear <shinianliuliang@163.com>
2015-07-17 14:39:54 +0800

    版本 0.0.1

---
+++ TenYmarqueeViewTests/TenYmarqueeViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  TenYmarqueeViewTests.swift
+//  TenYmarqueeViewTests
+//
+//  Created by tenyear on 15/7/17.
+//  Copyright (c) 2015年 tenyear. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TenYmarqueeViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
